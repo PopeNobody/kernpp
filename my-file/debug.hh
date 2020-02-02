396a03d3041243310af889b8e1be9984ecb33edd
Nobody <rich.freeman.paul@gmail.com>
2020-02-01 21:14:48 -0500

    a

--- my-file/debug.hh
+++ my-file/debug.hh
@@ -3,7 +3,6 @@
 
 #include <c_str.hh>
 #include <fmt.hh>
-#include <errno.h>
 
 void _debug(const char *file, unsigned line, const char *name, fmt::hex_t val);
 void _debug(const char *file, unsigned line, const char *name, size_t val );
@@ -17,28 +16,5 @@ void _debug(const char *file, unsigned line, const char *name, time_t val );
 #ifndef trace
 #define trace() do{ ; } while(0)
 #endif
-
-namespace sys {
-  typedef int errno_t;
-  const c_str &strerror(errno_t err=errno);
-  void perror(const c_str &msg1, const c_str &msg2);
-  void pexit(const c_str &msg1, const c_str &msg2);
-  void perror(const c_str &msg1);
-  void pexit(const c_str &msg1);
-  void do_warn_fail(
-      const char *file, unsigned line, 
-      bool res, const char *text,
-      bool getbetter
-      );
-};
-
-#define POS __FILE__,__LINE__
-
-#define warnif(x) sys::do_warnif(POS,x,#x,true)
-#define dieif(x) sys::do_warnif(POS,x,#x,true)
-
-#define dieunless(x) dieif(!x)
-#define warnunless(x) warnif(!x)
-
 
 #endif
