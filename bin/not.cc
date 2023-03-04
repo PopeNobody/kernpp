82054577cbf29be53c0b0ddcc2814b19577070d8
nobody <github2@invisiblehand.church>
2023-03-04 12:40:29 -0500

    Now I know where the environement is!

--- bin/not.cc
+++ bin/not.cc
@@ -3,7 +3,6 @@
 #include <syscall.hh>
 #include <getopt.hh>
 
-using namespace fmt;
 
 char def1[]="/bin/echo";
 char def2[]="/bin/sh";
@@ -14,68 +13,7 @@ char *defs[] = {
 #define wtermsig(res) (res&0x7f)
 #define wexitstatus(res) ((res&0xff00)>>8)
 
-
-static char to_char(int num)
-{
-  return num+'0';
-};
-int count(char**arr) 
-{
-  //buf.println("arr: ",(void*)arr);
-  int c=0;
-  while(arr[c])
-    ++c;
-  return c;
-};
-
-typedef char * str_t;
-typedef const char *str_c;
-
-static char tab[]="0123456789abcdef";
-
-template<size_t size>
-class char_buf{
-  char b[size];
-  char e[1];
-  char *p;
-  public:
-  char_buf(){
-    memset(b,'.',sizeof(b));
-    e[0]=0;
-  };
-  void put(char ch){
-    *--p=ch;
-  }
-  void reset(){
-    p=&e+1;
-    put(0);
-  }
-  char* fmt(bool neg,unsigned long val,int radix,int width,bool nl=false)
-  {
-    if(nl)
-      put('\n');
-    while(p>b) {
-      *--p=tab[val%radix];
-      if(val<radix)
-        break;
-      val/=radix;
-    }
-    char *fin=e-width;
-    while(p>fin)
-      *--p='.';
-    return p;
-  }
-};
-char_buf<32> buf;
-
-const char* env[]={
-  "HOME=/home/nn",
-    "PATH=/home/nn/bin:/usr/games:/home/nn/perl5/bin:"
-    "/home/nn/.cargo/bin:/opt/bin:/usr/local/sbin:"
-    "/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/games",
-    0
-};
-
+using namespace fmt;
 int main(int argc, char** argv, char **envp)
 {
   if(argc<2) {
