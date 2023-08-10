bbf612eba6ea7dadb4fb8c91234bdc99b8152f38
nobody <github2@invisiblehand.church>
2023-08-10 13:37:49 -0400

    x

--- bin/lspath.cc
+++ bin/lspath.cc
@@ -1,14 +1,49 @@
 #include <syscall.hh>
 using sys::write;
 
-int main(int argc, char**argv,char **envp) {
-  ++argv;
-  if(*argv)
-    write(1, *argv++);
-  while(*argv) {
-    write(1,L(" "));
-    write(1, *argv++);
+
+template<typename itr_t>
+struct range_t {
+  typedef itr_t mitr_t;
+  typedef const itr_t citr_t;
+
+  typedef typeof(*mitr_t()) mref_t;
+  typedef typeof(*citr_t()) cref_t;
+  struct data_t {
+    mitr_t b;
+    mitr_t e;
+    data_t(mitr_t b, mitr_t e)
+      : b(b), e(e)
+    {
+    };
+  } data;
+  range_t(mitr_t b, mitr_t e)
+    :data(b,e)
+  {
+  }
+  citr_t beg() const {
+    return data.b;
   };
-  write(1, L("\n"));
+  citr_t end() const {
+    return data.e;
+  };
+  size_t size() const {
+    return data.e-data.b;
+  };
+
+  mitr_t beg() {
+    return data.b;
+  };
+  mitr_t end() {
+    return data.e;
+  };
+};
+
+int main(int argc, char**argv,char **envp) {
+  typedef range_t<typeof(argv)> argv_r;
+  argv_r args(argv,argv+argc);
+  for(auto b(args.beg()), e(args.end()); b!=e; b++){
+
+  };
   return 0;
 };
