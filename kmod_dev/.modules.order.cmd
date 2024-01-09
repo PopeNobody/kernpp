962e7f6307e9aa203cec98078cfbe59b02fcd464
nobody <github2@invisiblehand.church>
2024-01-09 05:46:46 -0500

    x

--- kmod_dev/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nn/src/cpp/kernpp/new/kmod_dev/modules.order := {   echo /home/nn/src/cpp/kernpp/new/kmod_dev/pstree.ko; :; } | awk '!x[$$0]++' - > /home/nn/src/cpp/kernpp/new/kmod_dev/modules.order
