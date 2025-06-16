bc8750c13c2d8209930634164cc2cb50093ce65f
nobody <github2@invisiblehand.church>
2025-06-16 06:34:51 -0400

    x

--- bin/not.cc
+++ bin/not.cc
@@ -3,41 +3,35 @@
 #define wifexited(res) (wtermsig(res)==0)
 #define wtermsig(res) (res&0x7f)
 #define wexitstatus(res) ((res&0xff00)>>8)
+namespace sys {
+  ssize_t write(fd_t fd, int val)
+  {
+    char buf[19];
+    char end[1]={0};
+    char *pos=end;
+    do {
+      val/=10;
+      *--pos='0'+val%10;
+    } while(val);
+    return write(fd,pos,end-pos);
+  };
+}
 using sys::write;
-#include "itoa.hh"
-int write_dec(int fd, unsigned long val, bool neg=false){
-  return write(fd,sys::itoa(val,neg));
-};
-int main(int argc, char** argv, char **envp)
-{
-  if(argc<2) {
-    write(2,"usage: ");
-    write(2,argv[0]);
-    write(2," <command>\n");
-    return 2;
-  };
-  for(int i=0;envp[i];i++){
-    write(2,envp[i]);
-    write(2,"\n");
-  };
-  write(2,"(null)\n\n");
-  
-  write(2,"\n\nargc=");
-  write_dec(2,argc);
-  write(2,"\n\n");
 
-  pid_t opid=sys::getpid();
-  pid_t npid=sys::fork();
+int main(int argc, const char** argv, const char **envp)
+{
   int res;
-  if(npid) {
+  if(sys::fork()) {
     if(sys::wait(&res)<0)
       sys::pexit("wait");
-    write(2,"\n\n");
-    write_dec(2,res);
-    write(2,"\n");
   } else {
-    ++argv;
-    sys::execve(argv[0],argv, envp);
+    if(argv[1]) {
+      ++argv;
+    } else {
+      static const char *args[]={ "/bin/echo", "no args passed to not", 0 };
+      argv=args;
+    };
+    sys::execve(argv[0],(char*const*)argv, (char*const*)envp);
     sys::pexit("execve");
   };
   sys::exit(!res);
