50f7f74ce0870735e05137114aea57d7829872e7
nobody <github2@invisiblehand.church>
2025-06-16 06:02:20 -0400

    x

--- etc/resolve.mk
+++ etc/resolve.mk
@@ -1,20 +1,21 @@
-var/VV:=$(.VARIABLES)
 c++=ii dd SS oo
 asm=oo
 beg=c++ asm
 c++/cc:=$(wildcard */*.cc)
-c++/ii:=$(c++/cc:=.cc.ii)
-c++/dd:=$(c++/cc:=.cc.dd)
-c++/SS:=$(c++/cc:=.cc.ii)
-c++/oo:=$(c++/cc:=.cc.oo)
-cxx/--:=$(foreach x,$(c++),$$(c++/$x))
-asm/SS:=$(wildcard */*.SS)
-asm/oo:=$(asm/SS:=.SS.oo);
-var/NN:=$(filter-out $(var/VV),$(.VARIABLES))
-show=$(eval $$(warning $($1)))
-$(warning $show)
-#    $(foreach x,$(var/nn),$(call show, 
-#v:=asm
-#$(eval $$(warning $v=$$($v)))
+c++/ii:=$(c++/cc:=.ii)
+c++/dd:=$(c++/cc:=.dd)
+c++/SS:=$(c++/cc:=.SS)
+c++/oo:=$(c++/cc:=.oo)
+asm/SS:=$(wildcard */*.S)
+asm/oo:=$(asm/SS:=.o)
 
-tst:;
+bin/c++:=$(filter bin/%,$(c++/cc:.cc=))
+bin/asm:=$(filter bin/%,$(asm/SS:.S=))
+all: $(bin/c++) $(bin/asm)
+#    $(warning $(bin/c++))
+#    $(warning $(bin/asm))
+lib/asm:=$(filter lib/%,$(asm/oo))
+lib/c++:=$(filter lib/%,$(c++/oo))
+#    $(warning $(lib/c++))
+#    $(warning $(lib/asm))
+lib/obj:=$(lib/asm) $(lib/c++)
--- etc/rules.mk
+++ etc/rules.mk
@@ -2,19 +2,25 @@
 $(lib/lib): $(lib/obj)
 	ar -r "$@" $(lib/obj) $(lib/xxx)
 
-%: %.oo etc/ld_flags $(lib/lib)
-	ld -o "$@" $< $(lib/lib)
-
-%.ii: %.cc  etc/cppflags
+$(c++/ii): %.cc.ii: %.cc  etc/cppflags
 	rm -f $*.ii $*.oo $*.S
 	g++ -E -o "$@" $< @etc/cppflags -MD -MT $@ -MF $@.d
 
-%.S: %.ii  etc/cxxflags
+$(c++/SS): %.SS: %.ii  etc/cxxflags
 	rm -f $*.S $*.oo
 	g++ -S -o "$@" $< @etc/cxxflags -MD -MT $@ -MF $@.d
 
-%.oo: %.S  etc/asmflags
+$(c++/oo): %.oo: %.SS  etc/asmflags
 	as -o "$@" $< @etc/asmflags
+
+$(bin/c++): %: %.cc.oo etc/ld_flags $(lib/lib)
+	ld -o "$@" $< $(lib/lib)
+
+$(bin/asm): %: %.S.o
+	ld -o "$@" $<
+
+$(asm/oo): %.S.o: %.S etc/asmflags
+	as -o $@ $<
 
 %/run: %
 	./$<
