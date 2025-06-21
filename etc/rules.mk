258fb90d7507309abdfdebc00ee7410f1f8816f8
nobody <github2@invisiblehand.church>
2025-06-21 01:07:54 -0400

    x

--- etc/rules.mk
+++ etc/rules.mk
@@ -1,18 +1,18 @@
 
 $(lib/lib): $(lib/obj)
-	ar -r $@ $(lib/obj) $(lib/xxx)
+	sbin/ar $@ $(lib/obj) $(lib/xxx)
 
-$(all/exe): %: %.oo etc/ld_flags $(lib/lib)
-	ld -o $@ $< $(lib/lib)
+$(all/exe): %: %.cc.oo etc/ld_flags $(lib/lib)
+	sbin/ld $@ $< $(lib/lib)
 
-$(all/cpp): %.ii: %.cc  etc/cppflags
-	g++ -E -o $@ $< @etc/cppflags -MD -MT $@ -MF $@.dd
+$(all/cpp): %.cc.ii: %.cc  etc/cppflags
+	sbin/g++ $@ $< @etc/cppflags -MD -MT $@ -MF $@.dd
 
 $(gen/asm): %.SS: %.ii  etc/cxxflags
-	g++ -S -o $@ $< @etc/cxxflags -MD -MT $@ -MF $@.dd
+	sbin/g++ $@ $< @etc/cxxflags -MD -MT $@ -MF $@.dd
 
 $(all/obj): %.oo: %.SS  etc/asmflags
-	as -o $@ $< @etc/asmflags
+	sbin/as $@ $< @etc/asmflags
 
 
 %/run: %
