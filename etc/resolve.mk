d55c71d3a273cc1eb651bb95cd0a6dd99bffa75c
nobody <github2@invisiblehand.church>
2025-06-21 01:28:26 -0400

    x

--- etc/resolve.mk
+++ etc/resolve.mk
@@ -1,19 +1,26 @@
+define nl
+
+
+
+endef
+
 dir/src:=bin lib tst
 src/c++:=$(foreach d,$(dir/src),$(wildcard $d/*.cc))
 src/asm:=$(foreach d,$(dir/src),$(wildcard $d/*.S))
-src/mod:=$(src/c++) $(src/asm)
+bin/exe:=$(filter bin/%,$(src/c++:.cc=) $(src/asm:.S=))
+tst/exe:=$(filter tst/%,$(src/c++:.cc=) $(src/asm:.S=))
+$(warning $(nl) SOURCES: )
 SHOW=$(eval $$(warning $1:  $$($1)))
-$(foreach v,src/c++ src/asm,$(call SHOW,$v))
-
-#$(warning $(call SHOW,src/c++))
-#src/exe:=$(filter bin/%,$(src/mod)) $(filter tst/%,$(src/mod))
-#all/c++:=$(src/c++)
-#all/dep:=$(src/c++:=.dd)
-#all/cpp:=$(all/c++:=.ii)
-#gen/asm:=$(src/c++:=.SS)
-#all/asm:=$(src/asm) $(gen/asm)
-#all/obj:=$(all/asm:=.oo)
-#lib/obj:=$(filter lib/%,$(all/obj))
-#all/exe:=$(src/exe)
-#
-#all: $(all/exe)
+$(foreach v,src/c++ src/asm src/mod,$(call SHOW,$v))
+$(warning $(nl) EXECS:)
+$(foreach v,bin/exe tst/exe,$(call SHOW,$v))
+tgt/exe:=$(bin/exe) $(tst/exe)
+tgt/cpp:=$(src/c++:=.ii)
+tgt/dep:=$(tgt/cpp:=.dd)
+tgt/asm:=$(src/c++:=.SS)
+tgt/obj:=$(src/c++:=.oo) $(src/asm:=.oo)
+$(warning $(nl) TARGETS:)
+$(foreach v,tgt/cpp tgt/asm tgt/obj tgt/exe tgt/dep,$(call SHOW,$v))
+tgt/all:=$(foreach v,cpp asm obj exe dep,$(tgt/$v))
+$(warning $(nl)$(nl)$(nl) ALL TARGETS:)
+$(foreach v,tgt/all,$(call SHOW,tgt/all))
