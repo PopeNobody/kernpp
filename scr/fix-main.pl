9275dc01af01f3091aced152b68e9846c5a2954b
nobody <github2@invisiblehand.church>
2025-06-29 09:18:49 -0400

    x

--- scr/fix-main.pl
+++ scr/fix-main.pl
@@ -9,6 +9,7 @@ use Nobody::Util;
 use Nobody::PP;
 our(@VERSION) = qw( 0 1 0 );
 open(STDERR,">&STDOUT");
+use Digest::MD5;
 
 for my $ARGV( glob("*/*.cc") ) {
   local($_)=$ARGV;
@@ -17,6 +18,7 @@ for my $ARGV( glob("*/*.cc") ) {
   while(<STDIN>){
     if((@_=split(m{^\s*(int\s+main\s*\(.*\))\s*}))>1){
       $_[1]="int main(int argc,char *const*argv,char *const*envp)";
+      @_=grep { defined && length } @_;
       $_="@_";
     };
     print;
@@ -27,5 +29,10 @@ for my $ARGV( glob("*/*.cc") ) {
   open(STDOUT,">&STDERR");
   for($ARGV) {
     system("diff $_ $_.new");
+    if($?) {
+      system("mv $_.new $_");
+    } else {
+      system("rm $_.new");
+    };
   };
 };
