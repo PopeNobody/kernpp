5b09d5275331fa872526f13ebf0f2effece50851
nobody <github2@invisiblehand.church>
2025-07-16 01:23:25 -0400

    x

--- scr/genheaders.pl
+++ scr/genheaders.pl
@@ -1,70 +1,38 @@
-#!/usr/bin/env perl
+#/usr/bin/perl
 
-use common::sense;
-use autodie;
-use Nobody::Util;
+use strict;
+use warnings;
+use autodie qw(:all);
+use Data::Dumper;
+$|++;
 use FindBin;
 use lib "$FindBin::Bin";
 
-our (%calls);
-require "syscall.pl";
 
-sub code_sort { $calls{$a}{code} <=> $calls{$b}{code} }
-say "#include \"syscall.low.hh\"";
-say "namespace sys {";
-say "";
-say "// Auto-generated syscall wrappers";
+our(%calls);
+require "syscall.pl" or die "$@";
 
-for my $name (sort { code_sort } keys %calls) {
-    our (%call);
-    local *call = $calls{$name};
-    next unless $call{impl};
+sub code_sort {
+	return $calls{$a}{code} <=> $calls{$b}{code};
+};
+$\="\n";
+$,=" ";
 
-    my @args = @{ $call{args} };
-    my @decl_args = map { join(" ", @$_) } @args;
-    push @decl_args, "errhand_t hand";
-    my $args_str_with_default = join(", ", @decl_args);
-    my $args_str = join(", ", map { join(" ", @$_) } @args);
-    my $args_err = "errhand_t hand";
-    my $rtype = $call{ret};
-    my $code = $call{code};
-    my $sysname = $call{name};
-
-    my $attr = "__attribute__((__always_inline__))";
-    if ($call{noreturn}) {
-        $attr = "__attribute__((__always_inline__, __noreturn__))";
-    }
-
-    my $arity = scalar(@args);
-    $arity = 6 if $arity > 6;
-
-    my @arg_names = map { $_->[1] } @args;
-    my $arg_list = join(", ", map { "(uint64_t)$_" } @arg_names);
-    for($arg_list) {
-      $_=", $_" if length;
-    };
-    for($args_str) {
-      $_=length?"$_, $args_err":$args_err;
-    };
-    say "";
-    say "";
-    say "// __NR__ $sysname = $code";
-    say "inline $rtype $name($args_str_with_default = err_log)";
-    say "  $attr;";
-    say "";
-    say "";
-    say "inline $rtype $name($args_str) {";
-    say "  uint64_t res = syscall$arity($code $arg_list);";
-    say "";
-    say "";
-
-    if ($call{noreturn}) {
-        say "  __builtin_unreachable();";
-    } else {
-        say "  return chk_return<$rtype>(res, hand);";
-    }
-    say "}";
-}
-
-say "";
-say "} // namespace sys";
+print q(#file __FILE__);
+print q(#line __LINE__);
+print q(extern "C" {);
+for my $name (sort {code_sort} keys %calls) {
+	our(%call);
+	local *call=$calls{$name};
+	next unless $call{impl};
+	my @args = @{$call{args}};
+	for(@args) {
+		$_=join(" ",@$_);
+	};
+	@args=join(", ", @args);
+	my $rtype=$call{ret};
+        print "// __NR__", $call{name}, "=", $call{code}, "\n";
+	print "inline $rtype $name(@args)";
+	print "  __attribute__((__always_inline__));";
+};
+print q(});
